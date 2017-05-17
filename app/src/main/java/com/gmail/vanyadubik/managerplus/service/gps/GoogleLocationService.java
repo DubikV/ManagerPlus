b8ee96ae3c95ae2ce32c85b638195dcfb8d798fb
Dubik Ivan <vanyadubik@gmail.com>
2017-05-17 08:50:34 +0300

    merge

--- app/src/main/java/com/gmail/vanyadubik/managerplus/service/gps/GoogleLocationService.java
+++ app/src/main/java/com/gmail/vanyadubik/managerplus/service/gps/GoogleLocationService.java
@@ -18,7 +18,6 @@ import com.google.android.gms.location.LocationServices;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static com.gmail.vanyadubik.managerplus.common.Consts.MIN_DISTANCE_WRITE_TRACK;
 import static com.gmail.vanyadubik.managerplus.common.Consts.MIN_SPEED_WRITE_LOCATION;
 import static com.gmail.vanyadubik.managerplus.common.Consts.MIN_TIME_WRITE_TRACK;
 import static com.gmail.vanyadubik.managerplus.common.Consts.TAGLOG_GPS;
