0d01bd0b080ec465c4b5d900a948e2a8f72fcbf6
DubikV <vanyadubik@gmail.com>
2017-10-04 20:17:48 +0300

    fix boot act

--- app/src/main/java/com/gmail/vanyadubik/managerplus/activity/BootAct.java
+++ app/src/main/java/com/gmail/vanyadubik/managerplus/activity/BootAct.java
@@ -8,8 +8,11 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.gmail.vanyadubik.managerplus.R;
+import com.gmail.vanyadubik.managerplus.app.ManagerPlusAplication;
 
 public class BootAct extends AppCompatActivity {
+
+    private static int TIME_START_APP = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +20,30 @@ public class BootAct extends AppCompatActivity {
         setContentView(R.layout.activity_splashscreen);
 
         final ImageView imageView = (ImageView) findViewById(R.id.splash);
-        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotating);
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate_right);
         imageView.startAnimation(animation);
         Thread splashTread = new Thread() {
             @Override
             public void run() {
+                try {
+                    int waited = 0;
 
-                Intent intent = new Intent(BootAct.this, StartActivity.class);
-                startActivity(intent);
-                finish();
+                    ((ManagerPlusAplication) getApplication()).getComponent().inject(BootAct.this);
+
+                    while (waited < TIME_START_APP) {
+                        sleep(100);
+                        waited += 100;
+                    }
+
+                    Intent intent = new Intent(BootAct.this, StartActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                    BootAct.this.finish();
+                } catch (InterruptedException e) {
+                } finally {
+                    BootAct.this.finish();
+                }
 
             }
         };
